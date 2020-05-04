53bee963e25075a88f02357677e1658065c7d3c7
RCristyan <rcristyan9@gmail.com>
2020-05-03 22:24:40 -0300

    Corrigidos problemas de compatibilidade.
    Signed-off-by: RCristyan <rcristyan9@gmail.com>
    Co-authored-by: moacirmsj <moacirmascarenha86@gmail.com>

---
+++ projeto/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Header from './screens/Header/index.js';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen name="Header" component={Header} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+} 
