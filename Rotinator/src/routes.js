40adcf325699c6d40245b19f646988a524f7ac74
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-05-04 21:52:58 -0300

    Iniciada tela estatica home.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: Renan Cristyan Araújo Pinheiro <rcristyan9@gmail.com>

--- Rotinator/src/routes.js
+++ Rotinator/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Header from './screens/Header/index.js';
+import Home from './screens/Home/index.js'
 
 const AppStack = createStackNavigator();
 
@@ -10,7 +10,7 @@ export default function Routes() {
     return (
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown: false }}>
-                <AppStack.Screen name="Header" component={Header} />
+                <AppStack.Screen name="Home" component={Home} />
             </AppStack.Navigator>
         </NavigationContainer >
     );
---
+++ Rotinator/src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Header from '../Header';
+
+
+export default function Home() {
+    return (
+        <View>
+            <Header />
+            <Text>Rotinator HOME</Text>
+        </View>
+    );
+
+}
