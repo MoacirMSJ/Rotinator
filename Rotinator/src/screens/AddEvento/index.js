4e9517e81bd7a6c15e1c7fe9b095ec813366c73b
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-05-23 04:37:23 -0300

    Concluída tela estática para adicionar evento.

--- Rotinator/src/screens/AddEvento/index.js
+++ Rotinator/src/screens/AddEvento/index.js
@@ -1,62 +1,63 @@
 import React, {useState,useEffect } from 'react';
-import { View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Image, Text,TextInput,ScrollView, Platform,TouchableOpacity } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import Header from '../Header'
+import Header from '../Header';
+import perto from '../../../assets/perto.png';
+import verifica from '../../../assets/verifica.png';
 
 import styles from './styles';
 
 export default function AddEvento() {
     const [titulo, useTitulo] = useState('padrão');
-    const [inicio, setInicioHora] = useState();
     const [notificacaoInicio, setNotificacaoInicio] =useState('');
+    const [notificacaoFinal, setNotificacaoFinal] = useState('');
     const [date, setDate] = useState(new Date(1598051730000));
+    const [dateFinal, setDateFinal] = useState(new Date(1598051730000));
+    const [opcao, setOpcao] = useState(1);
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
-    const [template, setTemplate] = useState('00:00');
-    const aux = "balaba";
-    // const [inicio, setInicioHora] = useState();
-    // const [titulo, useTitulo] = useState('padrão');
-    // const [titulo, useTitulo] = useState('padrão');
+    const [templateI, setTemplateI] = useState('00:00');
+    const [templateF, setTemplateF] = useState('00:00');
+
+
     useEffect(() => {
         let hora = String(date.getHours()).length ===1? '0'+ String(date.getHours()) : String(date.getHours());
         let min = String(date.getMinutes()).length ===1 ? '0'+String(date.getMinutes()) : String(date.getMinutes());
-        setTemplate(hora +':'+ min);
-    },[date]);
+        let horaf = String(dateFinal.getHours()).length ===1? '0'+ String(dateFinal.getHours()) : String(dateFinal.getHours());
+        let minf = String(dateFinal.getMinutes()).length ===1 ? '0'+String(dateFinal.getMinutes()) : String(dateFinal.getMinutes());
+        setTemplateI(hora +':'+ min);
+        setTemplateF(horaf +':'+ minf);
+    },[date, dateFinal]);
 
     const onChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
         setShow(Platform.OS === 'ios');
-        setDate(currentDate);
+        if(opcao === 1){
+            setDate(currentDate);
+            setOpcao(2);
+        }
+        if(opcao===2){
+            setDateFinal(currentDate);
+            setOpcao(1);
+        }
+        
     };
     
-      const showMode = currentMode => {
+    const showMode = currentMode => {
         setShow(true);
         setMode(currentMode);
-      };
-    const mostrarRelogio = () => {
+    };
+    const mostrarRelogio =()=>{
         showMode('time');
-      };
-
-    return (
-        <View>
-            <Header />
-                <Text style={styles.container}>Adicionar Evento</Text>
-            <View style={styles.titulo}>
-                <Text>Titulo:</Text>
-                <TextInput
-                 style={styles.tituloEntrada}
-                    onChangeText= {text => useTitulo(text)}
-                    value= {titulo}
-                    maxLength={25}
-                />
-            </View>
-            <View style={styles.titulo}>
-                <Text>Inicio:</Text>                
-                < TouchableOpacity style={styles.horaButton} onPress={mostrarRelogio}>
-                    <Text style={styles.textoHora}>{template}</Text>    
-                </ TouchableOpacity>
-            </View>
-            {show && (
+    };
+    const mostrarRelogio2 =()=>{
+        setOpcao(2)
+        showMode('time');
+    };
+    
+    function Relogio(){
+        if(opcao==1){
+            return(
                 <DateTimePicker
                 testID="hora"
                 timeZoneOffsetInMinutes={0}
@@ -65,20 +66,85 @@ export default function AddEvento() {
                 is24Hour={true}
                 display="default"
                 onChange={onChange}
-                />)}
-            <View style={styles.mensagemContainer}>
-                <Text>Notificacao inicial:</Text>
-                <TextInput
-                 style={styles.mensagem}
-                    onChangeText= {text => setNotificacaoInicio(text)}
-                    value= {notificacaoInicio}
-                    maxLength={35}
                 />
-            </View>
-            <View>
-                <Text>Final:</Text>
-            </View>
+            )
+        }
+        else{
+            return(
+                <DateTimePicker
+                testID="hora"
+                timeZoneOffsetInMinutes={0}
+                value={dateFinal}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChange}
+                />
+            )
+        }
+        
+    }
 
-        </View>
+    function salvar(){
+        alert("salvar")
+    }
+    function excluir(){
+        alert("excluir")
+    }
+    return (
+            <ScrollView style={styles.container} >
+                <Header />
+                    <Text style={styles.tituloTela}>Adicionar Evento</Text>
+                <View style={styles.titulo}>
+                    <Text style={styles.texto}>Título:</Text>
+                    <TextInput
+                    style={styles.tituloEntrada}
+                        onChangeText= {text => useTitulo(text)}
+                        value= {titulo}
+                        maxLength={25}
+                    />
+                </View>
+                <View style={styles.titulo}>
+                    <Text style={styles.texto}>Inicio:</Text>                
+                    < TouchableOpacity style={styles.horaButton} onPress={mostrarRelogio} >
+                        <Text style={styles.textoHora}>{templateI}</Text>    
+                    </ TouchableOpacity>
+                </View>
+                {show && Relogio()}
+                <View style={styles.mensagemContainer}>
+                    <Text style={styles.texto}>Notificação inicial</Text>
+                    <TextInput
+                    style={styles.mensagem}
+                        onChangeText= {text => setNotificacaoInicio(text)}
+                        value= {notificacaoInicio}
+                        maxLength={35}
+                    />
+                </View>
+                <View style={styles.titulo}>
+                    <Text style={styles.texto}>Final:</Text>                
+                    < TouchableOpacity style={styles.horaButton} onPress={mostrarRelogio2}>
+                        <Text style={styles.textoHora}>{templateF}</Text>    
+                    </ TouchableOpacity>
+                </View>
+                <View style={styles.mensagemContainer}>
+                    <Text style={styles.texto}>Notificação final</Text>
+                    <TextInput
+                    style={styles.mensagem}
+                        onChangeText= {text => setNotificacaoFinal(text)}
+                        value= {notificacaoFinal}
+                        maxLength={35}
+                    />
+                </View>
+                <View style={styles.caixaBotoes}>
+
+                    < TouchableOpacity onPress={excluir}>
+                            <Image style={styles.iconeSalvar} source={perto}/>
+                    </ TouchableOpacity>
+                    < TouchableOpacity onPress={salvar}>
+                            <Image style={styles.iconeExcluir} source={verifica}/>
+                    </ TouchableOpacity>
+                </View>
+            </ScrollView>
+
     );
 }
--- Rotinator/src/screens/AddEvento/styles.js
+++ Rotinator/src/screens/AddEvento/styles.js
@@ -2,56 +2,94 @@ import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-
     container:{
+        flex:1,
+        marginBottom: 5
+    },
+    tituloTela:{
         flexDirection: 'row',
         justifyContent: 'center',
         textAlign: 'center',
         fontWeight: 'bold',
+        fontSize: 20,
+        marginTop: 10
     },
+    texto:{
+        fontWeight:'bold',
+        paddingBottom: 10,
+        fontSize: 15
+    }
+    ,
     titulo:{
         flexDirection: 'row',
-        paddingTop: 10,
+        paddingTop: 25,
         marginLeft:10,
+        
         // justifyContent: 'center'
     },
     tituloEntrada: {
         textAlign:'center',
         fontWeight:'bold',
         marginLeft: 10,
-        borderColor: 'gray',
+
+        borderColor: "#000000",
         backgroundColor: '#d3d7de',
         borderWidth: 1,
         borderRadius: 3,
-        width: "80%"
+        width: "80%",
     },
     horaButton:{
         marginLeft:10,
         borderColor: "#000000",
         borderWidth: 1,
+        borderRadius:3,
         width: 60,
         backgroundColor: "#d3d7de",
-        textAlign: 'center'
+        textAlign: 'center',
+    
+        
     },
     textoHora:{
         fontWeight:'bold',
-        textAlign:"center"
+        textAlign:"center",
+        fontSize: 20
     },
-    mensagemContainer:{
-        flexDirection: 'row',
-        paddingTop: 10,
+    mensagemContainer:{    
+        // flexDirection: 'row',
+        paddingTop: 25,
         marginLeft:10,
     },
     mensagem:{
         textAlign:'center',
         fontWeight:'bold',
-        marginLeft: 10,
-        borderColor: 'gray',
+        marginLeft: 50,
         backgroundColor: '#d3d7de',
         borderWidth: 1,
         borderRadius: 3,
-        width: "60%",
+        width: "80%",
+        borderColor: "#000000",
+        borderWidth: 1,
         
+    },
+    caixaBotoes:{
+        flexDirection:'row',
+        // borderWidth: 1,
+        // borderColor: 'gray',
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+        height: 100
+    },
+    iconeSalvar:{
+        width: 35,
+        height: 35,
+        marginRight: 30,
+    },
+    iconeExcluir:{
+        width: 35,
+        height: 35,
+        marginLeft: 30,
     }
     
 });
